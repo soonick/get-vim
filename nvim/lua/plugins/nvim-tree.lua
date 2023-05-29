69318cb60e59f443949b5859497e03dc9cc07500
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2023-05-29 11:47:52 -0300

    Nvim tree opens correctly when using tabnew with file name. Fix tab_drop on nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,17 +1,17 @@
 local function open_nvim_tree()
-  require("nvim-tree.api").tree.open()
+  require('nvim-tree.api').tree.open()
 end
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
 
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
+    'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require("nvim-tree").setup({
+    require('nvim-tree').setup({
       actions = {
         remove_file = {
           close_window = true,
@@ -24,28 +24,52 @@ return {
           return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
         end
 
+        local function file_exists(name)
+          local f = io.open(name, 'r')
+          if f ~= nil then
+            io.close(f)
+            return true
+          else
+            return false
+          end
+        end
+
         -- This part removes f and F mappings which conflict with telescope
         api.config.mappings.default_on_attach(bufnr)
         vim.keymap.del('n', 'f', { buffer = bufnr })
         vim.keymap.del('n', 'F', { buffer = bufnr })
 
         -- Opens nvim-tree when a new tab is created
-        local openTreeGrp = vim.api.nvim_create_augroup("AutoOpenTree", { clear = true })
-        vim.api.nvim_create_autocmd("TabNew", {
-          command = "NvimTreeFindFile",
+        local openTreeGrp = vim.api.nvim_create_augroup('AutoOpenTree', { clear = true })
+        vim.api.nvim_create_autocmd('TabNew', {
+          callback = function(ev)
+            if ev.file ~= '' and file_exists(ev.match) then
+              api.tree.open({
+                path = ev.match
+              })
+            else
+              if ev.file == '' then
+                -- I still don't know how to make a tab with no name open with
+                -- nvim tree, so for now we just open it alone
+              else
+                vim.cmd('wincmd l')
+                api.tree.open()
+              end
+            end
+          end,
           group = openTreeGrp,
         })
 
         -- Closes nvim-tree if it's the last open buffer
         vim.o.confirm = true
-        vim.api.nvim_create_autocmd("BufEnter", {
-          group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+        vim.api.nvim_create_autocmd('BufEnter', {
+          group = vim.api.nvim_create_augroup('NvimTreeClose', {clear = true}),
           callback = function()
-            local layout = vim.api.nvim_call_function("winlayout", {})
-            if layout[1] == "leaf" and
-                vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
+            local layout = vim.api.nvim_call_function('winlayout', {})
+            if layout[1] == 'leaf' and
+                vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), 'filetype') == 'NvimTree'
                 and layout[3] == nil then
-              vim.cmd("quit")
+              vim.cmd('quit')
             end
           end
         })
@@ -57,11 +81,12 @@ return {
         local swap_then_open_tab = function()
           local node = api.tree.get_node_under_cursor()
           if node.type == 'file' then
-            vim.cmd("wincmd l")
+            -- Swith to right buffer
+            vim.cmd('wincmd l')
           end
-          api.node.open.tab(node)
+          api.node.open.tab_drop(node)
         end
-        vim.keymap.set("n", "<CR>", swap_then_open_tab, opts("Tab drop"))
+        vim.keymap.set('n', '<CR>', swap_then_open_tab, opts('Tab drop'))
       end
     })
   end,
